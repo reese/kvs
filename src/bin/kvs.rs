d4bde7af90bcf4fe86326ba9d5fedf5ae15615b8
Reese Williams <rtwill722@gmail.com>
2020-01-12 12:05:13 -0500

    Implement enough to pass all tests.

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,28 @@
+#[macro_use(load_yaml)]
+extern crate clap;
+use clap::App;
+use kvs::KvStore;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("./cli.yml");
+    let arg_matches = App::from_yaml(yaml).get_matches();
+
+    let (subcommand, _subcommand_matches) = arg_matches.subcommand();
+
+    match subcommand {
+        "get" => {
+            eprintln!("unimplemented");
+            exit(123);
+        },
+        "set" => {
+            eprintln!("unimplemented");
+            exit(123);
+        },
+        "rm" => {
+            eprintln!("unimplemented");
+            exit(123);
+        },
+        _ => panic!("Unrecognized argument."),
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,23 @@
-// empty for now
+use std::collections::HashMap;
+
+pub struct KvStore {
+    store: HashMap<String, String>,
+}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        return KvStore { store: HashMap::new() };
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        self.store.insert(key, value);
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        return self.store.get::<String>(&key).cloned();
+    }
+
+    pub fn remove(&mut self, key: String) {
+        self.store.remove::<str>(key.as_ref());
+    }
+}
