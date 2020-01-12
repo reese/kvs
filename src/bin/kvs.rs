d0ea35f82609ed10331dff97222f9d23cab864bd
Reese Williams <rtwill722@gmail.com>
2020-01-12 12:20:39 -0500

    Set package info from Cargo env variables.

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -6,8 +6,12 @@ use std::process::exit;
 
 fn main() {
     let yaml = load_yaml!("./cli.yml");
-    let arg_matches = App::from_yaml(yaml).get_matches();
-
+    let app = App::from_yaml(yaml)
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .name(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"));
+    let arg_matches = app.get_matches();
     let (subcommand, _subcommand_matches) = arg_matches.subcommand();
 
     match subcommand {
